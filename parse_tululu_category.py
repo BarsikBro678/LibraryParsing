cbb844f45ce7bb6f287fa256b8a65493d0683baa
BarsikBro678 <barsik.bro.7711@gmail.com>
2024-07-27 17:43:32 +0300

    Добавил аргументы --dest_folder, --skip_txt и skip_imgs

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -8,10 +8,11 @@ from pathvalidate import sanitize_filename
 
 from main import download_txt, download_image, parse_book_page
 
-def write_books_args(books_args, filename="books_args"):
+
+def write_books_args(books_args, filename="books_args", folder=""):
     filename = sanitize_filename(filename)
     filename = filename.strip()
-    with open(f"{filename}.json", "w", encoding="utf8") as file:
+    with open(f"{folder}{filename}.json", "w", encoding="utf8") as file:
         json.dump(books_args, file, ensure_ascii=False)
 
 
@@ -23,11 +24,21 @@ def main():
                         help="Страница начиная с которой скачиваются книги о фантастике. Стандартное значение - 1.")
     parser.add_argument("--end_page", default=10, type=int,
                         help="Страница на которой книги уже не скачиваются. Стандартное значение - 10.")
-
+    parser.add_argument("--dest_folder", default="", type=str,
+                        help="Указывать ли путь до файла.")
+    parser.add_argument("--skip_imgs", action="store_true",
+                        help="Не скачивать картинки.")
+    parser.add_argument("--skip_txt", action="store_true",
+                        help="Не скачивать книги.")
     args = parser.parse_args()
     start_page = args.start_page
     end_page = args.end_page
+    dest_folder = args.dest_folder
+    skip_imgs = args.skip_imgs
+    skip_txt = args.skip_txt
     books_args = []
+    if dest_folder != "":
+        os.makedirs(dest_folder)
     for page in range(start_page, end_page):
         url = f"https://tululu.org/l55/{page}"
         response = requests.get(url)
@@ -41,8 +52,10 @@ def main():
             book_response = requests.get(book_url)
             book_response.raise_for_status()
             book = parse_book_page(book_response, book_id[2:])
-            download_txt(response, book["filename"])
-            download_image(book["image_url"], book["image_path"])
+            if not skip_txt:
+                download_txt(response, book["filename"], folder = f"{dest_folder}books/")
+            if not skip_imgs:
+                download_image(book["image_url"], book["image_path"], folder = f"{dest_folder}images/")
             book_args = {"title": book["title"].replace("\xa0", ""),
                          "author": book["author"].replace("\xa0", ""),
                          "img_src": book["image_src"].replace("\xa0", ""),
@@ -51,7 +64,8 @@ def main():
                          "genres": list(map(lambda x: x.replace("\xa0", ""), book["genres"])),
             }
             books_args.append(book_args)
-    write_books_args(books_args)
+    write_books_args(books_args, folder = dest_folder)
+
 
 if __name__ == "__main__":
     main()
